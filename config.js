2011e9b7f4537ad8853546d9c1826c63157ca7ca
Alex3236 <45303195+alex3236@users.noreply.github.com>
2022-08-20 09:24:30 +0800

    稍微改改（

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 const basic = { // 基础设置
     favicon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌿️</text></svg>', // 页面图标
     title: '(～￣▽￣)～', // 页面标题
-    avatar: '2706992599', // 头像 URL，也可用 QQ 号 (e.g. 2706992599) 或 Github 用户名 (e.g. @alex3236)
+    avatar: '2706992599', // 头像 URL，也可用 QQ 号 (e.g. 10000) 或 Github 用户名 (e.g. @alex3236)
     name: 'Alex3236', // 头像下的昵称
     sign: '<strike>我很可爱，请给我钱</strike> ( •̀ ω •́ )✧', // 个性签名或提示文字，可使用 HTML 格式
     user_page: 'https://github.com/alex3236', // 点击头像或名字时跳转的链接，留空或删除则不跳转
@@ -12,7 +12,7 @@ const basic = { // 基础设置
 }
 
 const theme = { // 主题设置
-    page_bg: '#c3d7df', // 网页背景（十六进制，或图片 URL）
+    page_bg: '#d7f3f8', // 网页背景（十六进制，或图片 URL）
     card_bg: '#ffffffcc', // 卡片背景色（十六进制，可带透明度，不能是 URL）
     qrcode_bg: '#eaeffde6', // 二维码背景色（十六进制，可带透明度，不能是 URL）
     qrcode_fg: '#335eea' // 二维码颜色（十六进制，可带透明度，不能是 URL）
