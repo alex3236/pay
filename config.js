df45ac588f5119dfd45f513d3e08e6cee5e3b1bd
Alex3236 <45303195+alex3236@users.noreply.github.com>
2022-08-22 21:50:12 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 const basic = { // 基础设置
     favicon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌿️</text></svg>', // 页面图标
     title: '(～￣▽￣)～', // 页面标题
-    avatar: '2706992599', // 头像 URL，也可用 QQ 号 (e.g. 10000) 或 Github 用户名 (e.g. @alex3236)
+    avatar: 'https://s2.loli.net/2022/08/22/OptHQwyxKUVcaT7.jpg', // 头像 URL，也可用 QQ 号 (e.g. 10000) 或 Github 用户名 (e.g. @alex3236)
     name: 'Alex3236', // 头像下的昵称
     sign: '<strike>我很可爱，请给我钱</strike> ( •̀ ω •́ )✧', // 个性签名或提示文字，可使用 HTML 格式
     user_page: 'https://github.com/alex3236', // 点击头像或名字时跳转的链接，留空或删除则不跳转
