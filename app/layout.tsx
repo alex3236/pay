445ba66d9554db49b578e07a4608d2b36205ef5a
Alex3236 <45303195+alex3236@users.noreply.github.com>
2025-05-25 00:30:38 +0800

    fix: wrong `lang` field

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
     return (
-        <html lang="en">
+        <html lang="zh-CN">
             <body>
                 {children}
                 <Toaster richColors position="top-center" />
